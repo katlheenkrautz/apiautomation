cff02c763fc44c71024e84427f0b95f627fca59c
Katlheen Krautz <98427130+katlheenkrautz@users.noreply.github.com>
2025-05-29 09:05:20 -0500

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,6 @@
+BASE_URL = "https://cnt-d6be727c-3e42-4584-87d3-17257db66d50.containerhub.tripleten-services.com"
+CREATE_USER_URL = f"{BASE_URL}/api/v1/users"
+CREATE_KIT_URL = f"{BASE_URL}/api/v1/kits"
+
+
+

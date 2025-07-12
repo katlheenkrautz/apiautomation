1e6e11594c921a4a45afcce178d19bef190e178a
Katlheen Krautz <98427130+katlheenkrautz@users.noreply.github.com>
2025-07-12 15:19:18 -0500

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -2,12 +2,16 @@ import requests
 import configuration
 import data
 
-# Función para crear un nuevo usuario
+# Function to send a POST request to create a new user
 def create_new_user(body):
+    # Sends a POST request to the user creation endpoint with the provided request body and headers
     return requests.post(configuration.CREATE_USER_URL, json=body, headers=data.headers)
 
-# Función para crear un nuevo kit
+# Function to send a POST request to create a new kit for a user
 def post_new_client_kit(kit_body, auth_token):
+    # Copy the default headers and add the Authorization token
     headers_with_auth = data.headers.copy()
     headers_with_auth["Authorization"] = f"Bearer {auth_token}"
+
+    # Sends a POST request to the kit creation endpoint with the provided kit body and auth headers
     return requests.post(configuration.CREATE_KIT_URL, json=kit_body, headers=headers_with_auth)

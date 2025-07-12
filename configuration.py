4a49679723656a9c1a42649807afabb15e01234b
Katlheen Krautz <98427130+katlheenkrautz@users.noreply.github.com>
2025-07-12 15:07:29 -0500

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,6 +1,13 @@
+# BASE_URL is the base URL of the server. 
+# To get it, you need to start the server, copy the URL it provides, and paste it here. (This is just an example.)
 BASE_URL = "https://cnt-d6be727c-3e42-4584-87d3-17257db66d50.containerhub.tripleten-services.com"
+
+# CREATE_USER_URL is the full endpoint used to send requests to create new users via the API.
 CREATE_USER_URL = f"{BASE_URL}/api/v1/users"
+
+# CREATE_KIT_URL is the full endpoint used to send requests to create new kits via the API.
 CREATE_KIT_URL = f"{BASE_URL}/api/v1/kits"
+
 
 
 

13c8c6e1381a36e802c81b84fc2e79e5ff9bf29d
Katlheen Krautz <98427130+katlheenkrautz@users.noreply.github.com>
2025-07-12 15:18:16 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,13 +1,16 @@
+# Headers used in HTTP requests to specify that the content type is JSON
 headers = {
     "Content-Type": "application/json"
 }
 
+# Default request body for creating a new user
 user_body = {
-    "firstName": "Andrea",
-    "phone": "+11234567890",
-    "address": "123 Elm Street, Hilltop"
+    "firstName": "Andrea",  # User's first name
+    "phone": "+11234567890",  # User's phone number
+    "address": "123 Elm Street, Hilltop"  # User's address
 }
 
+# Default request body for creating a new kit
 kit_body = {
-    "name": "a"
+    "name": "a"  # Kit name (can be modified during testing)
 }
